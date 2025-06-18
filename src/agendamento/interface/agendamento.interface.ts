32ddad3549406aa434384e32ade2c2dc31675cce
ThallesLana <thalles.lana1@gmail.com>
2025-06-18 16:58:36 -0300

    :tada: init: initial commit include configs and dto and interface of project.

---
+++ src/agendamento/interface/agendamento.interface.ts
@@ -0,0 +1,10 @@
+export interface IAgendamentoPayload {
+    medico: string;
+    paciente: string;
+    data_horario: string;
+}
+
+export interface IAgendamentoResponse {
+    mensagem: string;
+    agendamento: IAgendamentoPayload;
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const slsw = require("serverless-webpack");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+    context: __dirname,
+    mode: slsw.lib.webpack.isLocal ? 'development': 'production',
+    entry: slsw.lib.entries,
+    devtool: slsw.lib.webpack.isLocal ? 'cheap-module-eval-source-map' : 'source-map',
+    resolve: {
+        extensions: ['.mjs', '.json', '.ts', '.js'],
+    },
+    output: {
+        libraryTarget: 'commonjs',
+        path: path.join(__dirname, '.webpack'),
+        filename: '[name].js',
+    },
+    target: 'node',
+    externals: [nodeExternals()],
+    module: {
+        rules: {
+            test: /\.ts$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/,
+        }
+    }
+}
