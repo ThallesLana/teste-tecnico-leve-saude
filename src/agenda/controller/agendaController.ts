a7af5960a3923ba4bbb59aebe5979a5055f93ae6
ThallesLana <thalles.lana1@gmail.com>
2025-06-22 22:02:42 -0300

    :recycle: refactor: update the return of agenda controller

--- src/agenda/controller/agendaController.ts
+++ src/agenda/controller/agendaController.ts
@@ -1,28 +1,18 @@
 import { APIGatewayProxyResult } from 'aws-lambda';
 import { AgendaService } from '../service/agendaService';
+import { formatApiResponse } from '../../utils/formatApiResponse';
 
 export const getAgendas = async (): Promise<APIGatewayProxyResult> => {
   try {
     const agendaService = new AgendaService();
     const agendas = agendaService.getAgendas();
 
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: JSON.stringify(agendas),
-    };
+    return formatApiResponse(200, JSON.stringify(agendas));
   } catch (error) {
     console.error('Erro ao buscar agendas:', error);
-    return {
-      statusCode: 500,
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: JSON.stringify({ message: 'Erro interno do servidor ao buscar agendas.' }),
-    };
+    return formatApiResponse(
+      500,
+      JSON.stringify({ message: 'Erro interno do servidor ao buscar agendas.' }),
+    );
   }
 };
