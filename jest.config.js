602010bea88139cce79d8992a21f01012c8ef899
ThallesLana <thalles.lana1@gmail.com>
2025-06-22 22:14:23 -0300

    :wrench: chore: change the jest to modules, fix param coverageReporters and include the dir in .gitignore.

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    converageReporters: ["json", "lcov", "text", "clover"]
-}
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+};
